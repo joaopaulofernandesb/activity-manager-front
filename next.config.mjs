b1feeb4730296d69c9622fa6a917fa1ccdf21c8f
João Brasil <joao.brasil@gazin.com.br>
2024-09-23 22:37:14 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/* eslint-disable @typescript-eslint/no-require-imports */
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development', // Desabilita o PWA no ambiente de desenvolvimento
+    register: true,
+    skipWaiting: true,
+  });
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = withPWA({
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
-        appDir: true,
-      },
-    pwa: {
-      dest: "public",
-    }
-  };
+      appDir: true,
+    },
+  });
   
   export default nextConfig;
+  
