08092640f7510d82071d5f9724fa0d0d21e23a58
João Brasil <joao.brasil@gazin.com.br>
2024-09-24 10:37:01 -0300

    Initializing gh-pages branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Activity Manager",
   description: "Gerenciador de atividades para o desenvolvimento",
   generator: "Next.js",
-  manifest: "/manifest.json",
+  manifest: "/activity-manager-front/manifest.json",
   keywords: ["nextjs", "next14", "pwa", "next-pwa"],
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
   authors: [
@@ -30,8 +30,8 @@ export const metadata: Metadata = {
   viewport:
     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
-    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
-    { rel: "icon", url: "icons/icon-128x128.png" },
+    { rel: "apple-touch-icon", url: "/activity-manager-front/icons/icon-128x128.png" },
+    { rel: "icon", url: "/activity-manager-front/icons/icon-128x128.png" },
   ],
 };
 
