dbae70aeeaa763f3aeff12ae52dfec646317c273
João Brasil <joao.brasil@gazin.com.br>
2024-09-23 22:20:46 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  dest: "public", // Local onde o service worker será gerado
-  disable: process.env.NODE_ENV === "development", // Desabilita o PWA no desenvolvimento
-  register: true,
-  skipWaiting: true,
-});
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    pwa: {
+      dest: "public",
+    }
+  };
+  
+  export default nextConfig;
