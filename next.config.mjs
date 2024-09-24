6554d45f697bfcf98e45af2814315673979f07c5
João Brasil <joao.brasil@gazin.com.br>
2024-09-24 10:03:48 -0300

    Initializing gh-pages branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/activity-manager-front',  // O caminho base do seu projeto no GitHub Pages
-  assetPrefix: '/activity-manager-front/',  // Prefixo para os arquivos estáticos
-};
+  basePath: '/activity-manager-front',
+  assetPrefix: '/activity-manager-front/',
+  pwa: {
+    dest: 'public',  // Define onde o service worker será gerado
+    register: true,  // Se deve registrar o service worker automaticamente
+    skipWaiting: true,  // Permite que o SW seja ativado assim que for instalado
+  },
+});
 
 export default nextConfig;
